2986c223f6ea437ed2002654b36768648e52550f
Thai Lam <thaivanlam171@gmail.com>
2024-11-17 17:32:33 +0700

    add customed css location file

--- index.php
+++ index.php
@@ -19,6 +19,8 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
     <link href="../img/favicon.ico" rel="icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link rel="stylesheet" type="text/css" href="home.css" />
 </head>
 
 <body>
