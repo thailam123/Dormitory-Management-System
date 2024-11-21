7534a2d915bc9aea95f68eca5ee87b3478c10650
hornseller <d0888864666@gmail.com>
2024-11-21 22:04:43 +0700

    create message connection database

---
+++ message/connection.php
@@ -0,0 +1,6 @@
+<?php
+$sname = "localhost";
+$uname = "root";
+$password = "";
+mysqli_connect(hostname: 'localhost', username: 'root', password: '');
+$conn = mysqli_connect(hostname: $sname, username: $uname, password: $password, database: 'DMS');
