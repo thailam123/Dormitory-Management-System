c71c02eeb587e0e70beac360490d54d9d002c3f4
Thai Lam <thaivanlam171@gmail.com>
2024-11-27 10:41:52 +0700

    change connect db

--- Student/Insert.php
+++ Student/Insert.php
@@ -1,5 +1,5 @@
 <?php
-include_once 'connection.php';
+include_once '../CommonMethods/connection.php';
 
 if (count($_POST) > 0) {
     $result = mysqli_query($conn, "INSERT INTO student (Stu_id , Name, DOB, Phone_number, Email, R_ID) 
