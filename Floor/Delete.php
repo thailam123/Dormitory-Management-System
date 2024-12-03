0653e1701239e9d09341c8e77677886f7e2da264
hornseller <d0888864666@gmail.com>
2024-12-03 21:44:57 +0700

    Update Delete.php

--- Floor/Delete.php
+++ Floor/Delete.php
@@ -1,9 +1,27 @@
 <?php
-include_once 'connection.php';
-$sql = "DELETE FROM floor WHERE Floor_Number='" . $_GET["Floor_Number"] . "'";
-if (mysqli_query(mysql: $conn, query: $sql)) {
-    include 'DispFloor.php';
+include_once '../CommonMethods/connection.php';
+
+if (isset($_GET["F_ID"])) {
+    $R_ID = $_GET["F_ID"];
+
+    $sql = "DELETE FROM floor WHERE F_ID = ?";
+
+    if ($stmt = mysqli_prepare(mysql: $conn, query: $sql)) {
+        mysqli_stmt_bind_param(statement: $stmt, types: "s", var: $R_ID);
+
+        if (mysqli_stmt_execute(statement: $stmt)) {
+            header(header: "Location: DispFloor.php");
+            exit();
+        } else {
+            echo "Error deleting record: " . mysqli_error(mysql: $conn);
+        }
+
+        mysqli_stmt_close(statement: $stmt);
+    } else {
+        echo "Error preparing the query: " . mysqli_error(mysql: $conn);
+    }
+
 } else {
-    echo "Error deleting record: " . mysqli_error(mysql: $conn);
+    echo "No Floor ID specified. $R_ID";
 }
 mysqli_close(mysql: $conn);
