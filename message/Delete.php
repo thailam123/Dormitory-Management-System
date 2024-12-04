ed39da830eb0fcd8e9cd8faa464c9c567c942b54
hornseller <d0888864666@gmail.com>
2024-12-04 23:04:03 +0700

    update delete message feature and add query sql file

--- message/Delete.php
+++ message/Delete.php
@@ -6,22 +6,23 @@ if (isset($_GET["ID"])) {
 
     $sql = "DELETE FROM message_table WHERE ID = ?";
 
-    if ($stmt = mysqli_prepare(mysql: $conn, query: $sql)) {
-        mysqli_stmt_bind_param(statement: $stmt, types: "s", var: $ID);
+    if ($stmt = mysqli_prepare($conn, $sql)) {
+        mysqli_stmt_bind_param($stmt, "s", $ID);
 
-        if (mysqli_stmt_execute(statement: $stmt)) {
-            header(header: "Location: DispMessage.php");
+        if (mysqli_stmt_execute($stmt)) {
+            header("Location: DispMessage.php");
             exit();
         } else {
-            echo "Error deleting record: " . mysqli_error(mysql: $conn);
+            echo "Error deleting record: " . mysqli_error($conn);
         }
 
-        mysqli_stmt_close(statement: $stmt);
+        mysqli_stmt_close($stmt);
     } else {
-        echo "Error preparing the query: " . mysqli_error(mysql: $conn);
+        echo "Error preparing the query: " . mysqli_error($conn);
     }
-
 } else {
-    echo "No Message ID specified. " . $ID;
+    echo "No Message ID specified.";
 }
-mysqli_close(mysql: $conn);
+
+mysqli_close($conn);
+?>
